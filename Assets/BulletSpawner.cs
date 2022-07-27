e6a844e678e14653c3284f3032151c0f2df8ad60
Red <lebruitdered@gmail.com>
2022-07-27 15:05:00 +0200

    Create real spawn

--- Assets/BulletSpawner.cs
+++ Assets/BulletSpawner.cs
@@ -5,10 +5,26 @@ using UnityEngine;
 public class BulletSpawner : MonoBehaviour
 {
     [SerializeField] GameObject _bulletPrefab;
+    [SerializeField] float _spawnCooldown;
+
+    float _lastShoot;
 
     void Update()
     {
-        GameObject.Instantiate(_bulletPrefab, transform.position, transform.rotation);
+        //Random.
+        float rx = Random.Range(-1f, 1f);
+        float ry = Random.Range(-1f, 1f);
+        Vector3 randomDirection = new Vector3(rx, ry);
+
+        if(Time.time > _lastShoot + _spawnCooldown)
+        {
+            _lastShoot = Time.time;
+            GameObject.Instantiate(_bulletPrefab, transform.position + randomDirection, transform.rotation);
+        }
+
     }
+
+
+
 
 }
