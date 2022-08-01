a437ed9dc7b18a798fb816ba657f951c0c48ef8b
Red <lebruitdered@gmail.com>
2022-08-01 16:41:29 +0200

    Add Gauge render update

--- Assets/BulletReceiver.cs
+++ Assets/BulletReceiver.cs
@@ -7,6 +7,11 @@ public class BulletReceiver : MonoBehaviour
     [SerializeField] int _bulletMax;        // Le score maximal atteignable pour notre objectif
     [SerializeField] float _idleDuration;   // La durée d'attente avant de baisser notre score
 
+    [Header("Gauge renderer")]
+    [SerializeField, ColorUsage(true, true)] Color _onSprite; // Sprites color in gauge
+    [SerializeField, ColorUsage(true, true)] Color _offSprite; // Sprites color in gauge
+    [SerializeField] SpriteRenderer[] _gauge; // Sprites in gauge
+
     int _currentScore;  // Le score actuel dans notre jeu
     float _lastBulletReceived;  // La date de la dernière reception d'une bullet
 
@@ -29,7 +34,7 @@ public class BulletReceiver : MonoBehaviour
 
     private void Update()
     {
-        // SI la date actuelle dépasse la date de la dernière bullet + duration
+        // Si la date actuelle dépasse la date de la dernière bullet + duration
         if(Time.time > _lastBulletReceived + _idleDuration)
         {
             // Alors on descend notre score
@@ -37,6 +42,23 @@ public class BulletReceiver : MonoBehaviour
             Debug.Log($" Current score {_currentScore}");
         }
 
+        // Combien on a rempli notre objectif ?
+        float percent = (float)_currentScore / (float)_bulletMax;
+        // En prenant ce pourcentage, ça représente combien de slot chez nous ?
+        float gaugeCompletion = percent * _gauge.Length;
+        Debug.Log(gaugeCompletion);
+        // Mettre à jour le rendu de notre compteur
+        for (int i = 0; i < _gauge.Length; i++)
+        {
+            if(i < gaugeCompletion)
+            {
+                _gauge[i].color = _onSprite;
+            }
+            else
+            {
+                _gauge[i].color = _offSprite;
+            }
+        }
     }
 
 }
