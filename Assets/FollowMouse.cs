f187fbe47cb980993bb234c320450a0c3de037b8
Red <lebruitdered@gmail.com>
2022-08-03 12:11:22 +0200

    Refactor the effector

--- Assets/FollowMouse.cs
+++ Assets/FollowMouse.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class FollowMouse : MonoBehaviour
 {
     [SerializeField] Camera _mainCamera;
+    [SerializeField] Transform _transformToMove;
 
     Vector2 _oldMousePosition;
-
     private void OnMouseDown()
     {
-        Debug.Log("Down");
+        Debug.Log("Follow mouse Down");
         _oldMousePosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void OnMouseDrag()
     {
+        Debug.Log("Follow mouse Drag");
         Vector2 currentMousePosition = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        Debug.Log($"MousePosition {Input.mousePosition} World {currentMousePosition}");
-        transform.Translate(currentMousePosition - _oldMousePosition, Space.World);
+        //Debug.Log($"MousePosition {Input.mousePosition} World {currentMousePosition}");
+        _transformToMove.Translate(currentMousePosition - _oldMousePosition, Space.World);
         // FIN
         _oldMousePosition = currentMousePosition;
     }
---
+++ Assets/InfluenceGizmo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfluenceGizmo : MonoBehaviour
+{
+
+    void OnMouseDown()
+    {
+        Debug.Log("Down on influencer");
+    }
+
+    void OnMouseDrag()
+    {
+        Debug.Log("Drag on Influencer");
+    }
+
+
+}
